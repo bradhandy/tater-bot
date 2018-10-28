94712a5a67464a9dc588e93e1a7cc1d6288056d0
Brad Handy <bhandy@vertafore.com>
2018-10-28 16:22:06 -0400

    * Adding debugging code to figure out what the local server port is when the failure occurs.

--- src/test/java/net/jackofalltrades/taterbot/util/LineCallback.java
+++ src/test/java/net/jackofalltrades/taterbot/util/LineCallback.java
@@ -6,6 +6,7 @@ import com.linecorp.bot.model.event.CallbackRequest;
 import com.linecorp.bot.model.event.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -21,22 +22,29 @@ public class LineCallback {
 
     private final WebTestClient webTestClient;
     private final LinePayloadEncoder linePayloadEncoder;
+    private final int localServerPort;
 
     public LineCallback(WebTestClient webTestClient, LinePayloadEncoder linePayloadEncoder) {
         this.webTestClient = webTestClient;
         this.linePayloadEncoder = linePayloadEncoder;
+        this.localServerPort = localServerPort;
     }
 
     public void submit(Event... events) throws JsonProcessingException {
         CallbackRequest callbackRequest = new CallbackRequest(Lists.newArrayList(events));
         String hmacHash = linePayloadEncoder.encodePayload(callbackRequest);
-        webTestClient.post()
-                .uri("/callback")
-                .accept(MediaType.APPLICATION_JSON)
-                .header("X-Line-Signature", hmacHash)
-                .syncBody(callbackRequest)
-                .exchange()
-                .expectStatus().isOk();
+        try {
+            webTestClient.post()
+                    .uri("/callback")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .header("X-Line-Signature", hmacHash)
+                    .syncBody(callbackRequest)
+                    .exchange()
+                    .expectStatus().isOk();
+        } catch (Exception e) {
+            System.err.println("Local Server Port at the time of failure:  " + localServerPort);
+            throw new RuntimeException(e);
+        }
     }
 
 }
